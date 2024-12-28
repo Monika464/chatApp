f808a1f134e1d051a82d5c7d044090687770eac2
Monika Kubianka <mkubianka@gmail.com>
2024-12-28 19:48:14 +0100

    socket.io basics

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicDirectoryPath));
+
+let count = 0;
+
+io.on("connection", (socket) => {
+  console.log("new web socked connection");
+
+  socket.emit("countUpdate", count);
+
+  // Obsługa zdarzenia z klienta
+  socket.on("increment", () => {
+    count++;
+    // socket.emit("countUpdate", count);
+    io.emit("countUpdate", count);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is up on port ${port}!`);
+});
