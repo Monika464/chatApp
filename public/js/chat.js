508ef5208796a1cbd828ae5af9de568b873c682e
Monika Kubianka <mkubianka@gmail.com>
2024-12-30 20:52:04 +0100

    geo link as a message

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,53 @@
-const socket = io("http://localhost:3000")
+const socket = io()
 
-document.querySelector("#message-form").addEventListener("submit", (e) => {
-  e.preventDefault()
-  // console.log("przycisk on");
-  //const message = document.querySelector("input").value;
-  const message = e.target.elements.message.value
-  //console.log("message consolelog", message);
+const form = document.getElementById("form")
+const input = document.getElementById("input")
+const messages = document.getElementById("messages")
 
-  socket.emit("sendMessage", message)
+document.querySelector("#send-location").addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("geolocation is not supported by your browser")
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    //console.log(position)
+    socket.emit("coords", {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    })
+  })
 })
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  if (input.value) {
+    console.log("input-value", input.value)
+    socket.emit("chat message", input.value)
+    input.value = ""
+  }
+})
+
+socket.on("chat message", (msg) => {
+  const item = document.createElement("li")
+  item.textContent = msg
+  messages.appendChild(item)
+  window.scrollTo(0, document.body.scrollHeight)
+})
+
+socket.on("hello", (arg) => {
+  console.log(arg) // 'world'
+})
+
+socket.on("ktos", (arg2) => {
+  console.log(arg2)
+})
+
+socket.on("ktosOff", (arg3) => {
+  console.log(arg3)
+})
+
+// socket.on("coords", (position) => {
+//   console.log("cord", position)
+// })
+
+socket.emit("hello", "jozek")
--- src/index.js
+++ src/index.js
@@ -1,31 +1,78 @@
-const path = require("path")
-const http = require("http")
 const express = require("express")
-const socketio = require("socket.io")
+const { createServer } = require("node:http")
+const { join } = require("node:path")
+const { Server } = require("socket.io")
 
 const app = express()
-const server = http.createServer(app)
-const io = socketio(server)
+const server = createServer(app)
+const io = new Server(server)
 
-const port = process.env.PORT || 3000
-const publicDirectoryPath = path.join(__dirname, "../public")
+app.use(express.static("public"))
 
-app.use(express.static(publicDirectoryPath))
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "../public/index.html"))
+})
 
 io.on("connection", (socket) => {
-  socket.emit("message", "Welcomme !")
-  socket.broadcast.emit("message", "A new user has joined")
+  console.log("a user connected")
 
-  socket.on("sendMessage", (message) => {
-    //console.log("socket otrzymal mesage", message);
-    io.emit("message", message)
+  socket.on("hello", (arg) => {
+    // console.log(arg) // 'world'
   })
+
+  socket.on("coords", (position) => {
+    //console.log(position)
+    io.emit(
+      "chat message",
+      `https://google.com/maps?q=${position.latitude}, ${position.longitude}`,
+    )
+  })
+
+  socket.broadcast.emit("chat message", "ktos dołaczył")
 
   socket.on("disconnect", () => {
-    io.emit("message", "A user has left")
+    console.log("user disconnected")
+    socket.broadcast.emit("chat message", "ktos sie rozłączył")
+  })
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg)
   })
 })
 
-server.listen(port, () => {
-  console.log(`Server is up on port ${port}!`)
+server.listen(3000, () => {
+  console.log("server running at http://localhost:3000")
 })
+////////////
+// const path = require("path")
+// const http = require("http")
+// const express = require("express")
+// const socketio = require("socket.io")
+
+// const app = express()
+// const server = http.createServer(app)
+// const io = socketio(server)
+
+// const port = process.env.PORT || 3000
+// const publicDirectoryPath = path.join(__dirname, "../public")
+
+// app.use(express.static(publicDirectoryPath))
+
+// io.on("connection", (socket) => {
+//   socket.emit("messageConts", "Welcomme !")
+//   socket.broadcast.emit("messageConst", "A new user has joined")
+
+//   socket.on("sendMessage", (message) => {
+//     //console.log("socket otrzymal mesage", message);
+//     io.emit("message", message)
+//   })
+
+//   socket.on("disconnect", () => {
+//     io.emit("message", "A user has left")
+//   })
+// })
+
+// server.listen(port, () => {
+//   console.log(`Server is up on port ${port}!`)
+// })
+//////////////
