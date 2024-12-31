791e2daca66545d1cb7f7e196ece20956489cabc
Monika Kubianka <mkubianka@gmail.com>
2024-12-31 21:13:14 +0100

    disabled message & geolocation button

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,12 +3,15 @@ const socket = io()
 const form = document.getElementById("form")
 const input = document.getElementById("input")
 const messages = document.getElementById("messages")
+const formButton = document.getElementById("form-button")
+const locationButton = document.getElementById("send-location")
 
-document.querySelector("#send-location").addEventListener("click", () => {
+//document.querySelector("#send-location").addEventListener("click", () => {
+locationButton.addEventListener("click", () => {
   if (!navigator.geolocation) {
     return alert("geolocation is not supported by your browser")
   }
-
+  locationButton.setAttribute("disabled", "disabled")
   navigator.geolocation.getCurrentPosition((position) => {
     //console.log(position)
     socket.emit(
@@ -18,6 +21,7 @@ document.querySelector("#send-location").addEventListener("click", () => {
         longitude: position.coords.longitude,
       },
       (coords) => {
+        locationButton.removeAttribute("disabled")
         console.log(coords)
       },
     )
@@ -26,7 +30,15 @@ document.querySelector("#send-location").addEventListener("click", () => {
 
 form.addEventListener("submit", (e) => {
   e.preventDefault()
+
+  formButton.setAttribute("disabled", "disabled")
+
+  if (!input.value.trim()) {
+    return formButton.removeAttribute("disabled")
+  }
+
   if (input.value) {
+    formButton.removeAttribute("disabled")
     //console.log("input-value", input.value)
     // socket.emit("chat message", input.value)
     socket.emit("chat message", input.value, (msg, error) => {
@@ -35,7 +47,9 @@ form.addEventListener("submit", (e) => {
       }
 
       console.log(msg)
+      input.focus()
     })
+
     input.value = ""
   }
 })
